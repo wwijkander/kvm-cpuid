b370a83a10ec34743e6426af5d37aa1aad3979d1
Wilhelm Wijkander <wilhelm@0x5e.se>
2023-11-15 14:42:56 +0100

    Show both physical and hypervisor vendors

--- cpuid.c
+++ cpuid.c
@@ -2,29 +2,38 @@
 #include<stdint.h>
 
 int main(){
+  int retval = 0;
   int eax;
   int a[3];
   const uint32_t base = 0x40000000;
   uint32_t leaf;
 
+  __asm__("mov %0, %%eax\n\t" :: "r" (0x0));
+  __asm__("cpuid\n\t");
+  __asm__("mov %%ebx, %0\n\t" : "=r" (a[0]));
+  __asm__("mov %%ecx, %0\n\t" : "=r" (a[2]));
+  __asm__("mov %%edx, %0\n\t" : "=r" (a[1]));
+  printf("Physical CPUID vendor: %s\n",&a);
+
   //for (leaf = base + 0xff00; leaf >= base; leaf -= 0x100) {
   for (leaf = base + 0xff00; leaf >= base; leaf -= 0x1) {
     __asm__("mov %0, %%eax\n\t" :: "r" (leaf));
     __asm__("cpuid\n\t");
-    __asm__("mov %%eax, %0\n\t" : "=r" (eax));
+//    __asm__("mov %%eax, %0\n\t" : "=r" (eax));
     __asm__("mov %%ebx, %0\n\t" : "=r" (a[0]));
     __asm__("mov %%ecx, %0\n\t" : "=r" (a[1]));
     __asm__("mov %%edx, %0\n\t" : "=r" (a[2]));
 
     if (a[2] > 0){
-      printf("leaf: %x\n",leaf);
-      printf("EAX dec: %d\n",eax);
-      printf("EAX: %x\n",eax);
-      printf("EBX: %x\n",a[0]);
-      printf("ECX: %x\n",a[1]);
-      printf("EDX: %x\n",a[2]);
-      printf("CPUID: %s\n\n",&a);
+      printf("Hypervisor CPUID vendor: %s\n",&a);
+ //     printf("leaf: %x\n",leaf);
+ //     printf("EAX dec: %d\n",eax);
+ //     printf("EAX: %x\n",eax);
+ //     printf("EBX: %x\n",a[0]);
+ //     printf("ECX: %x\n",a[1]);
+ //     printf("EDX: %x\n",a[2]);
+      retval = 2;
     }
   }
-  return  0;
+  return retval;
 }
